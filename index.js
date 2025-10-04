4ae09632d4007b31aaa1ffabb55b22308af67423
itsoktoxcl <itsok2excl@gmail.com>
2025-10-04 22:42:03 +0100

    to_do Initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const host = '192.168.1.226';
+const port = 2626;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
