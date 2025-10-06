ff745ae909c2acc04db64f3af9d7a8e014e9c757
itsoktoxcl <itsok2excl@gmail.com>
2025-10-06 07:06:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const http = require("http");
-const host = '0.0.0.0';
-const port = 2626;
-
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
-// adding comments
+    const clientInfo = `
+Client Information:
+==================
+IP Address: ${req.socket.remoteAddress}
+Port: ${req.socket.remotePort}
+HTTP Method: ${req.method}
+URL: ${req.url}
+HTTP Version: ${req.httpVersion}
+User-Agent: ${req.headers['user-agent']}
+Accept: ${req.headers['accept']}
+Accept-Language: ${req.headers['accept-language']}
+Accept-Encoding: ${req.headers['accept-encoding']}
+Connection: ${req.headers['connection']}
+Host: ${req.headers['host']}
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+All Headers:
+${JSON.stringify(req.headers, null, 2)}
+    `;
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(clientInfo);
+};
